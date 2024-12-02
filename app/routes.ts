a0b3ea1e67afd5e791178b9d1d1d298f58ddc485
Murthy Udupa <ugmurthy@gmail.com>
2024-12-01 17:36:23 -0800

    Initial commit with few sample routes

---
+++ app/routes.ts
@@ -0,0 +1,17 @@
+import { type RouteConfig, index ,route} from "@react-router/dev/routes";
+
+export default [
+    index("routes/home.tsx"),
+         route("component","routes/component.tsx"),
+         route("sample","routes/sample.tsx"),
+         //route("sampletypes","routes/sampletypes.tsx"),
+         route("samplesplat/*","routes/samplesplat.tsx"),
+         // routes with client loader
+         route("users/:pid","routes/users.pid.tsx"),
+         route("/users","routes/users.tsx"),
+         // routes with server loader 
+         route("susers/:pid","routes/susers.pid.tsx"),
+         route("/susers","routes/susers.tsx"),
+         
+
+        ] satisfies RouteConfig;
